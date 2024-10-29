865cbdbfe0348e124cb3b1cd596ec8486dbe92f5
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2024-10-30 05:41:03 +0800

    fix: canvas resize

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -8,19 +8,19 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
+      <hemisphereLight intensity={0.1} groundColor="black" />
       <spotLight
         position={[-20, 50, 10]}
-        angle={0.12}
-        penumbra={1}
-        intensity={1}
+        angle={0.2}
+        penumbra={0.5}
+        intensity={isMobile ? 0.8 : 1}
         castShadow
-        shadow-mapSize={1024}
+        shadow-mapSize={isMobile ? 512 : 1024} // Lower shadow quality on mobile
       />
-      <pointLight intensity={1} />
+      <pointLight intensity={0.9} />
       <primitive
         object={scene}
-        scale={isMobile ? 0.7 : 0.75}
+        scale={isMobile ? 0.6 : 0.75}
         position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
@@ -45,7 +45,7 @@ const ComputersCanvas = () => {
     <Canvas
       frameloop="demand"
       shadows
-      dpr={[1, 2]}
+      dpr={isMobile ? [1, 1.5] : [1, 2]} // Lower device pixel ratio on mobile for performance
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
