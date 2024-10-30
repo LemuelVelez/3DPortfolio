2a206c2e0b9065cd05dda556b83881f937bb34a9
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2024-10-30 20:51:13 +0800

    fix: modify responsiveness

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -6,9 +6,12 @@ import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
+    <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
+        <div
+          className="w-28 h-28 sm:w-24 sm:h-24 xs:w-20 xs:h-20"
+          key={technology.name}
+        >
           <BallCanvas icon={technology.icon} />
         </div>
       ))}
