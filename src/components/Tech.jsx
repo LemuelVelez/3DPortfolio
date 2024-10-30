8b11989f12567d912f770c6cbd4b27daef7bb2c5
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2024-10-30 21:58:30 +0800

    fix: change cases

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -8,10 +8,7 @@ const Tech = () => {
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology) => (
-        <div
-          className="w-28 h-28 sm:w-24 sm:h-24 xs:w-20 xs:h-20"
-          key={technology.name}
-        >
+        <div className="w-28 h-28" key={technology.name}>
           <BallCanvas icon={technology.icon} />
         </div>
       ))}
--- src/constants/index.js
+++ src/constants/index.js
@@ -59,27 +59,27 @@ const services = [
 
 const technologies = [
   {
-    name: "HTML 5",
+    name: "html",
     icon: html,
   },
   {
-    name: "CSS 3",
+    name: "css",
     icon: css,
   },
   {
-    name: "JavaScript",
+    name: "javascript",
     icon: javascript,
   },
   {
-    name: "TypeScript",
+    name: "typescript",
     icon: typescript,
   },
   {
-    name: "React JS",
+    name: "reactjs",
     icon: reactjs,
   },
   {
-    name: "Tailwind CSS",
+    name: "tailwind",
     icon: tailwind,
   },
   {
